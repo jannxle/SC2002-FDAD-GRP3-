22bae6f57439a8c0742edac14c948ba1d0c6b6e0
Matthew <yeezhekai@gmail.com>
2025-04-02 19:05:53 +0800

    test

--- BTO_application/src/main/Main.java
+++ BTO_application/src/main/Main.java
@@ -11,7 +11,6 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        System.err.println("sdfjfsg");
     	System.out.println("Working Directory: " + new File(".").getAbsolutePath());
 
     	//load user data
